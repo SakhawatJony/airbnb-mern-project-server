0704c7dd02d4f5ef58813ca66f9340f0a4e1eaaa
Sakhawat Hosen <sakhawathosen320@gmail.com>
2023-08-27 19:50:29 +0600

    depoly project

--- index.js
+++ index.js
@@ -10,18 +10,11 @@ app.use(cors());
 app.use(express.json());
 
 
-
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tw8naco.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+const client = new MongoClient(uri, {serverApi: {version: ServerApiVersion.v1,strict: true,deprecationErrors: true, }});
 
 async function run() {
   try {
@@ -39,12 +32,9 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+     }
 }
 run().catch(console.dir);
-
 
 app.get('/',(req,res)=>{
     res.send('airbnb-mern-project-server is sitting')
